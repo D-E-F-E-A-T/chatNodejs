9b03d5e64d8f69706c6f6a8f9ed9411ae2805998
cassianojr <cassiano.jr.2009@hotmail.com>
2018-03-23 17:38:39 -0300

    Optmizations with cluster for enable multi thread

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var app = require('./config/custom-express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cluster = require('cluster');
 
-app.set('io', io);
+var numWorkers = process.env.WEB_CONCURRENCY || 1;
+console.log(numWorkers);
 
-var port = process.env.PORT || 3000;
-var server = http.listen(port, function(){
+if(cluster.isMaster){
+    for(var i = 0; i<numWorkers; i++){
+        cluster.fork();
+    }
 
-});
+    cluster.on('listening', function(worker){
+        console.log('cluster conected: '+worker.process.pid);
+    });
+
+    cluster.on('exit', function(){
+        cluster.fork();
+    });
+}else{
+    app.set('io', io);
+    
+    var port = process.env.PORT || 3000;
+    var server = http.listen(port, function(){
+        if(!process.env.NODE_ENV){
+            console.log('chat version 1.0 running on '+port);
+        }
+    });
+}
